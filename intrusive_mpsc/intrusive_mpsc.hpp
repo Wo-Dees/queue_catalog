3da2230bc82188b878feb64db34f4b937c0c54e3
SenorLeopold <blockchain.leonid@icloud.com>
2023-12-08 16:18:56 +0000

    better

--- intrusive_mpsc/intrusive_mpsc.hpp
+++ intrusive_mpsc/intrusive_mpsc.hpp
@@ -3,7 +3,7 @@
 #include <atomic>
 
 template <class T>
-struct IntrusiveForwardNode { // CRTP
+struct IntrusiveForwardNode { // use CRTP
     IntrusiveForwardNode* next;
 
     T* AsItem() {
@@ -16,6 +16,19 @@ template <typename T>
 class MPSCIntrusiveQuque {
     using Node = IntrusiveForwardNode<T>;
 public:
+    void Push(Node* node) {
+        node->next = top_.load();
+
+        while (!top_.compare_exchange_weak(node->next, node)) {}
+    }
+
+    T* TryPop() {
+        auto* top = top_.load();
+
+        while (top && !top_.compare_exchange_weak(top, top->next)) {}
+
+        return top->AsItem();
+    }
 
 private:
     std::atomic<Node*> top_;
